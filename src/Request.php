a945f8e2f388d306e24e62f7377dc4c9e0dfe30a
Gergely Kreko <kreko.gergely@gmail.com>
2018-01-18 09:50:34 +0000

    Code standard improvements

--- src/Request.php
+++ src/Request.php
@@ -36,11 +36,22 @@ final class Request
         return self::$instance;
     }
 
-    private function __construct() {
+    /**
+     * Request constructor.
+     *
+     * @throws ApiException
+     */
+    private function __construct()
+    {
         $this->body = $this->getRequest();
     }
 
-    private function __clone() { }
+    /**
+     * @author WN
+     */
+    private function __clone()
+    {
+    }
 
     /**
      * @author WN
